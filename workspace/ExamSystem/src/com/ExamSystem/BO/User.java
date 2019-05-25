b0f0d5eddb53889b610321f9b9fd1e8de9473f1a
ayush-dubey <ayushdubey010@gmail.com>
2019-05-25 12:49:59 +0530

    added wildfly
    Wildfly

---
+++ workspace/ExamSystem/src/com/ExamSystem/BO/User.java
@@ -0,0 +1,32 @@
+package com.ExamSystem.BO;
+
+public class User {
+	String userName;
+	String name;
+	long userId;
+	String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
